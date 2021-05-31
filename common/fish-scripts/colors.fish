6669f4ed560c4371e18754507a0abd7ba0af2a67
McAllaster <5580190+McAllaster@users.noreply.github.com>
2021-05-31 01:53:20 -0400

    nyarlathotep config updates

---
+++ common/fish-scripts/colors.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/fish
+
+set -U fish_color_normal normal
+set -U fish_color_command magenta
+set -U fish_color_quote green
+set -U fish_color_redirection cyan
+set -U fish_color_end magenta
+set -U fish_color_error red
+set -U fish_color_param blue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator cyan
+set -U fish_color_escape cyan
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion green
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_comment yellow
+set -U fish_color_match --background=brblue
