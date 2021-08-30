4a2b3f22025e03767ff636e4a71f6725e679fd60
Mason McAllaster <mason@mcallaster.dev>
2021-08-29 22:40:18 -0400

    Reorganize/prune Nyarlathotep scripts
    Move scripts from Nyarlathotep directory to a common directory depending
    on usefulness, and remove those that aren't used anymore (old i3-related
    scripts).

--- nyarlathotep/scripts/pixelock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-maim /tmp/screen.png
-convert /tmp/screen.png -scale 2.5% -scale 4000% /tmp/screen.png
-[[ -f /home/user/images/icons/lock-icon.png ]]
-i3lock -i /tmp/screen.png
-rm /tmp/screen.png
-xset dpms force off
