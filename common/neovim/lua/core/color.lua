5ac85ca66fd0e7dabd86aa41b23d61f848c6367b
Mason McAllaster <mason@mcallaster.dev>
2022-04-22 15:44:46 -0400

    neovim, gruvbox

--- common/neovim/lua/core/color.lua
+++ common/neovim/lua/core/color.lua
@@ -1,5 +1,10 @@
 vim.o.termguicolors = true
 vim.o.background = "dark"
-vim.cmd("colorscheme gruvbox-material")
+
+vim.g.gruvbox_material_palette = "original"
 vim.g.gruvbox_material_enable_bold = true
 vim.g.gruvbox_material_enable_italic = true
+vim.g.gruvbox_material_transparent_background = true
+vim.g.gruvbox_material_better_performance = true
+
+vim.cmd("colorscheme gruvbox-material")
