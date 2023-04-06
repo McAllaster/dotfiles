bffdb1c816687842c908c53f5a5b0d50c74d0b31
Mason McAllaster <mason@mcallaster.dev>
2023-04-05 23:20:51 -0400

    migrate off linux/kitty

--- linux/fish-scripts/recard.fish
+++
@@ -1,6 +0,0 @@
-function recard --description "Restart smartcard daemon & gpg agent"
-  sudo systemctl restart pcscd.service
-  sudo systemctl restart pcscd.socket
-  sudo pkill -9 scdaemon
-  gpg-connect-agent reloadagent /bye
-end
