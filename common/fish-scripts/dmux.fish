0b8ddc2e02f2a516c8633e0af9563c482ee346b0
Mason McAllaster <mason@mcallaster.dev>
2024-10-17 16:17:55 -0400

    update dmux

--- common/fish-scripts/dmux.fish
+++ common/fish-scripts/dmux.fish
@@ -1,15 +1,15 @@
 function dmux --argument-names dir --description "Start a tmux development session"
-  switch "$dir"
-    case ""
-      set session (basename $PWD)
-	  set dir $PWD
-    case \*
-      set session (basename $dir)
-  end
+	switch "$dir"
+		case ""
+			set session (basename $PWD)
+			set dir $PWD
+		case \*
+			set session (basename $dir)
+	end
 
-  tmux new-session -d -s "$session" -n "edit" -c "$dir"; or return
-  tmux new-window -t "$session" -n "explore" -c "$dir"
-  tmux new-window -t "$session" -n "run" -c "$dir"
-  tmux split-window -h -t "$session:run" -c "$dir"
-  tmux select-window -t "$session" -t "edit"
+	tmux new-session -d -s "$session" -n "edit" -c "$dir"; or return
+	tmux new-window -t "$session" -n "explore" -c "$dir"
+	tmux new-window -t "$session" -n "run" -c "$dir"
+	tmux split-window -h -t "$session:run" -c "$dir"
+	tmux select-window -t "$session" -t "edit"
 end
